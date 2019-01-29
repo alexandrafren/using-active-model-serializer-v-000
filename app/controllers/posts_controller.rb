cce23d61e677a3fac3c1f1c1add641f2d2b7fec0
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-29 03:07:15 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @post, status: 200 }
-end
-    render json: @post, status: 200
+    end
   end
 
   def new
